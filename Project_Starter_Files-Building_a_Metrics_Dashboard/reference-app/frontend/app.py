529d7abab7b69130da44429c393cc3404c8a09b8
Ayman Moawad <aykamal@live.com>
2021-11-17 06:57:17 +0200

    Update app.py

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
 from prometheus_flask_exporter import PrometheusMetrics
 
+
+class InvalidUsage(Exception):
+    status_code = 400
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
 app = Flask(__name__)
 
 metrics = PrometheusMetrics(app, group_by='endpoint')
@@ -10,6 +24,40 @@ common_counter = metrics.counter(
     labels={'endpoint': lambda: request.endpoint}
 )
 
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.route("/403")
+@common_counter
+def status_code_403():
+    status_code = 403
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+@app.route("/404")
+@common_counter
+def status_code_404():
+    status_code = 404
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+@app.route("/500")
+@common_counter
+def status_code_500():
+    status_code = 500
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+@app.route("/503")
+@common_counter
+def status_code_503():
+    status_code = 503
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
 
 @app.route('/')
 @common_counter
